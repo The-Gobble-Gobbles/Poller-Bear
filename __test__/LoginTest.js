b4ec3086c4c8eb17b4f1530a00bdf3e18055cf72
Sbrown2018 <sara.brown07.sb@gmail.com>
2022-11-22 12:21:43 -0800

    Login Test

--- __test__/LoginTest.js
+++
@@ -1,5 +0,0 @@
-const request = require('supertest');
-const CreatePoll = require('../client/pages/Login.jsx')
-const server = 'http://localhost:3000';
-
-//need to click the button and see if we are correctly routed to the login page
