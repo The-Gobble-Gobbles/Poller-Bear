2d8c8b3917d4913a3807bb468a7c04ab914c0012
Sbrown2018 <sara.brown07.sb@gmail.com>
2022-11-22 10:27:25 -0800

    Co-authored-by: jemzir <jemzir@users.noreply.github.com> Co-authored-by: mcbrownc <mcbrownc@users.noreply.github.com> Co-authored-by: Gahl Peled <GP3-RS@users.noreply.github.com>

---
+++ __test__/supertest.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const CreatePoll = require('../client/pages/CreatePoll.jsx')
+const server = 'http://localhost:3000';
+
+
+describe('Route integration', () => {
+  describe('/', () => {
+    describe('GET', () => {
+      // Note that we return the evaluation of `request` here! It evaluates to
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
+      //the test isn't run until the it statements
+      it('responds with 200 status and text/html content type', () => {
+        return request(server)
+          .get('/')
+          .expect('Content-Type', /text\/html/)
+          .expect(200);
+      });
+    });
+  });
+});
